2c321be483ab8b57fd067d81dc2d93aac1306cfc
halizzy <69795942+halizzy@users.noreply.github.com>
2022-11-13 20:03:25 +0900

    파일명 바꾸기 및 오류 고치기

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const toggleBtn = document.querySelector('.navber_toggleBtn');
 const menu = document.querySelector('.navbar_menu');
 const icon = document.querySelector('.navbar_icon');
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     menu.classList.toggle('active');
     icon.classList.toggle('active');
 })
